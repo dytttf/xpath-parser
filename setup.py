0d58fa06d365646ce7915cf51eb18852c9cf2202
dyf <1821367759@qq.com>
2019-08-15 10:35:49 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xpath-parser",
-    version="0.0.1",
+    version="0.0.2",
     author="dytttf",
     author_email="1821367759@qq.com",
     description="A xpath expression parser",
@@ -14,9 +14,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dytttf/xpath-parser",
     packages=setuptools.find_packages(),
+    keywords="xpath,expression,parser",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
         "Operating System :: OS Independent",
     ],
 )
